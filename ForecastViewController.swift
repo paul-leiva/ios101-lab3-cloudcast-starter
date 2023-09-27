64f82e3e61bc8aa5b996f66d6a1af96c0ffbc00e
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2023-09-26 20:37:40 -0400

    Step 1: Creating a View Controller

---
+++ ForecastViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Paul Leiva on 9/26/23.
+//
+
+
+// Import the UIKit framework
+// You're almost always going to need this when your referencing UI elements in your file
+import UIKit
+
+// Class declaration, including the name of the class and its subclass (UIViewController)
+class ForecastViewController: UIViewController {
+
+    // Function override for the view controller
+    // This is fired when the view has finished loading for the first time
+    override func viewDidLoad() {
+        
+        // Some functions require you to call the super class implementation
+        // Always read the online documentation to know if you need to
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
