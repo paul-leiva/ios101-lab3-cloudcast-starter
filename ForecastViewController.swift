0f7ede74cef8da584864678fc431237da3d239e1
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2023-09-26 21:25:24 -0400

    Step 4: Linking UI elements to code

--- ForecastViewController.swift
+++ ForecastViewController.swift
@@ -13,6 +13,12 @@ import UIKit
 // Class declaration, including the name of the class and its subclass (UIViewController)
 class ForecastViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var forecastImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     // Function override for the view controller
     // This is fired when the view has finished loading for the first time
     override func viewDidLoad() {
