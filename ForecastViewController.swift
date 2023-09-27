36bafd2b59a4b8949360b153db39f34c0342744b
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2023-09-26 21:57:38 -0400

    Step 5: Adding mock data

--- ForecastViewController.swift
+++ ForecastViewController.swift
@@ -28,6 +28,21 @@ class ForecastViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Make sure the order of your parameters matches the order of your struct. The compiler will help you out here!
+        let fakeData = WeatherForecast(temperature: 25.0,
+                                        date: Date(),
+                                        weatherCode: .partlyCloudy)
+        configure(with: fakeData)
+    }
+    
+    private func configure(with forecast: WeatherForecast) {
+        forecastImageView.image = forecast.weatherCode.image
+        descriptionLabel.text = forecast.weatherCode.description
+        temperatureLabel.text = "\(forecast.temperature)°F"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, yyyy"
+        dateLabel.text = dateFormatter.string(from: forecast.date)
     }
     
 
